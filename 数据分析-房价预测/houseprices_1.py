14dd115d05f321fdbd5613436a9bc5cbf6b08f95
张俊阳 <949625150@qq.com>
2018-12-17 20:15:24 +0800

    update

---
+++ 数据分析-房价预测/houseprices_1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+ 
+train = pd.read_csv( 'train.csv') #读取train数据
+train_y = train.SalePrice
+predictor_x = ['LotArea','YearBuilt','OverallQual','1stFlrSF','FullBath'] #特征
+train_x = train[predictor_x]
+my_model = RandomForestRegressor() #随机森林模型
+ 
+my_model.fit(train_x,train_y) #fit
+test = pd.read_csv( 'test.csv') #读取test数据
+test_x = test[predictor_x]
+pre_test_y = my_model.predict(test_x)
+print(pre_test_y)
+ 
+my_submission = pd.DataFrame({'Id':test.Id, 'SalePrice':pre_test_y}) #建csv
+my_submission.to_csv('submission2.csv', index=False)
+
+
+
