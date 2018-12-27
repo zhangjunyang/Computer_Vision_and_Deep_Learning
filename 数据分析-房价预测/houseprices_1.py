d7d7dffd052e88760ac872826191f199306ecb74
张俊阳 <949625150@qq.com>
2018-12-27 14:08:17 +0800

    data

--- 数据分析-房价预测/houseprices_1.py
+++ 数据分析-房价预测/houseprices_1.py
@@ -1,20 +1,21 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
- 
-train = pd.read_csv( 'train.csv') #读取train数据
+
+train = pd.read_csv( '../input/train.csv') #读取train数据
 train_y = train.SalePrice
-predictor_x = ['LotArea','YearBuilt','OverallQual','1stFlrSF','FullBath'] #特征
+predictor_x = ['LotArea','YearBuilt','OverallQual','1stFlrSF','FullBath'] #选取五个特征
 train_x = train[predictor_x]
-my_model = RandomForestRegressor() #随机森林模型
- 
-my_model.fit(train_x,train_y) #fit
-test = pd.read_csv( 'test.csv') #读取test数据
+
+model = RandomForestRegressor() #随机森林模型
+model.fit(train_x,train_y) #fit
+
+test = pd.read_csv( '../input/test.csv') #读取test数据
 test_x = test[predictor_x]
-pre_test_y = my_model.predict(test_x)
+
+pre_test_y = model.predict(test_x)
 print(pre_test_y)
  
 my_submission = pd.DataFrame({'Id':test.Id, 'SalePrice':pre_test_y}) #建csv
 my_submission.to_csv('submission.csv', index=False)
-
 
 
